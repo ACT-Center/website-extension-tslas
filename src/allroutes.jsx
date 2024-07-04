14e466f7ca8bbf8ec54d6a9cd63e96031b8128f7
mesaaransh <mesaaransh@gmail.com>
2024-07-05 00:02:13 +0530

    home page complete

--- src/allroutes.jsx
+++ src/allroutes.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 
 
 export default function Allroutes()
 {
-    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>/ route</div>}>
-      <Route index element={<div>/ route</div>} />
+      <Route path="/">
+        <Route index element={<Home/>} />
         <Route path="home" element={<Home />} />
         <Route path="*" element={<div>no page route</div>} />
       </Route>
     </Routes>
-  </BrowserRouter>
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     console.log("home");
 
     return (
-        <div style={{ display: "flex" }}>
+        <div className="home">
             <Card section="mainsec1" heading="Reasearch Projects" title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. "/>
             <Card section="mainsec2" heading="Reasearch Projects" title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. "/>
             <Card section="mainsec3" heading="Reasearch Projects" title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. "/>
