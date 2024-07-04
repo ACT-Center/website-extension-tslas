a4faaeed6a165cfa18c488d4c599346c83a72b40
chirag bansal <chirageng.stu22@gmail.com>
2024-07-04 23:53:56 +0530

    routing not working solved

--- src/allroutes.jsx
+++ src/allroutes.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 
-
-export default function Allroutes()
-{
+export default function Allroutes() {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<div>/ route</div>}>
-      <Route index element={<div>/ route</div>} />
-        <Route path="home" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<div>no page route</div>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
+  );
 }
