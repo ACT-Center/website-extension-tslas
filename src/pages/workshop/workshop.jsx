f33a4249df046fb1e0ddeba557ffc5ab3334a924
chirag bansal <chirageng.stu22@gmail.com>
2024-08-22 15:43:15 +0530

    worlshop page data added

--- src/pages/workshop/workshop.jsx
+++ src/pages/workshop/workshop.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let conferenceDetailsList = [
     {
         facultyName: "Andrea Raimondi (co-organiser)",
@@ -30,72 +29,33 @@ let conferenceDetailsList = [
     }
 ];
 
-=======
-import { useOutletContext } from "react-router-dom";
-import workData from "./data.js";
-import { useEffect } from "react";
->>>>>>> aa78c7ce99f83c0ab867b3d3a33b5cc8fe010a17
-
 export default function Workshop() {
-
-    let [setTitle, setDesc] = useOutletContext()
-
-    useEffect(() => {
-        setTitle("Workshops & Conferences")
-        setDesc("Access Exclusive, Cutting-Edge Insights: Unlock the Latest Research and Industry Trends to Drive Innovation")
-    }, [])
 
     return (
         <div>
             <div className="projects">
-
-<<<<<<< HEAD
                 <h1>Workshop & Conference Organized</h1>
-                {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p> */}
-
                 <ol>
                     {
-                        conferenceDetailsList.map((e,index)=>(
-                            <li><Publication pub={{facultyname : e.facultyName , location : e.location , date : e.date , conference : e.conferenceDetails , website : e.website}}/></li>
+                        conferenceDetailsList.map((e, index) => (
+                            <li key={index}>
+                                <Publication pub={{ facultyname: e.facultyName, location: e.location, date: e.date, conference: e.conferenceDetails, website: e.website }} />
+                            </li>
                         ))
                     }
                 </ol>
-=======
-                <h1>Workshops</h1>
-
-                {
-                    workData.map((work) => (
-                        <Works pub={work}/>
-                    ))
-                }
->>>>>>> aa78c7ce99f83c0ab867b3d3a33b5cc8fe010a17
-
             </div>
         </div>
     );
 }
 
-<<<<<<< HEAD
 function Publication({ pub }) {
-
     return (
         <div className="pub">
             <h4>{pub.facultyname}</h4>
-            <p>Location :- {pub.location} , Date :- {pub.date} </p>
-            <p>Details of the conference :- {pub.conference}</p>
-            {pub.website == '' ? null : <a href={pub.website} target="blank">Website</a>}
-=======
-
-function Works({pub}){
-
-    return(
-        <div className="pub">
-            <h4>{pub.Title}</h4>
-            <p>By: {pub.FacultyName}</p>
-            <p>{pub.Organizer}, {pub.Location} </p>
-            {/* <p>{pub.Description}</p> */}
->>>>>>> aa78c7ce99f83c0ab867b3d3a33b5cc8fe010a17
+            <p>Location: {pub.location} , Date: {pub.date}</p>
+            <p>Details of the conference: {pub.conference}</p>
+            {pub.website && <a href={pub.website} target="_blank" rel="noopener noreferrer">Website</a>}
         </div>
-    )
-
+    );
 }
