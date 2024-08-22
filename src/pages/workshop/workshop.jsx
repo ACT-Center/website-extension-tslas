aa78c7ce99f83c0ab867b3d3a33b5cc8fe010a17
mesaaransh <mesaaransh@gmail.com>
2024-08-22 15:12:20 +0530

    data complete for workshops

--- src/pages/workshop/workshop.jsx
+++ src/pages/workshop/workshop.jsx
@@ -1,22 +1,43 @@
+import { useOutletContext } from "react-router-dom";
+import workData from "./data.js";
+import { useEffect } from "react";
+
 export default function Workshop() {
+
+    let [setTitle, setDesc] = useOutletContext()
+
+    useEffect(() => {
+        setTitle("Workshops & Conferences")
+        setDesc("Access Exclusive, Cutting-Edge Insights: Unlock the Latest Research and Industry Trends to Drive Innovation")
+    }, [])
+
     return (
         <div>
             <div className="projects">
 
-                <h1>2022</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <h1>Workshops</h1>
 
-                <ol>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ol>
+                {
+                    workData.map((work) => (
+                        <Works pub={work}/>
+                    ))
+                }
 
             </div>
         </div>
     );
+}
+
+
+function Works({pub}){
+
+    return(
+        <div className="pub">
+            <h4>{pub.Title}</h4>
+            <p>By: {pub.FacultyName}</p>
+            <p>{pub.Organizer}, {pub.Location} </p>
+            {/* <p>{pub.Description}</p> */}
+        </div>
+    )
+
 }
