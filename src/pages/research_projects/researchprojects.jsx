2bec96f7a4a804ef9d7a1500ea18e09de0bc16a7
chirag bansal <chirageng.stu22@gmail.com>
2024-08-22 15:35:36 +0530

    research projects

--- src/pages/research_projects/researchprojects.jsx
+++ src/pages/research_projects/researchprojects.jsx
@@ -1,23 +1,87 @@
 import "./projects.css"
 
-export default function Researchprojects()
-{
+let names = [
+    "Prof Santha Kumari",
+    "Prof Santha Kumari",
+    "Sheikh Adil Edrisi",
+    "Jappen Oberoi",
+    "Jappen Oberoi",
+    "Jappen Oberoi"
+];
+
+let projects = [
+    "An experimental Investigation of the Psychological determinants of Financial decision making: A prospect theory approach, Sponsoring Agency: ICSSR, Duration: 2013-2015, Status: Completed.",
+    "Santha Kumari (2019). Factors influencing resilience in children: The mediating role of protective and compensatory factors in coping with adversities. Funding Agency ICSSR (Indian Council of Social Science Research. 8 Lakhs Completed.",
+    "Sustainability assessment of urban forestry by managing urban marginal lands for developing green space",
+    "Red-penciling Ancient Indian History",
+    "The Ramayana Project",
+    "Reappraising Ashoka"
+];
+
+let fundingAgencies = [
+    "Indian Council of Social Science Research",
+    "Indian Council of Social Science Research",
+    "Science and Engineering Research Board, Govt. of India (SERB, GoI) New Delhi, India",
+    "Tiet Seed Grant",
+    "Tslas Research Fund",
+    "Tslas Research Fund"
+];
+
+let fundingAmounts = [
+    "7 lakhs",
+    "8 lakhs",
+    "25.99 lakhs",
+    "6.82 lakhs",
+    "4.50 lakhs",
+    "3 lakhs"
+];
+
+let projectDurations = [
+    "2013-2015",
+    "2019-2021",
+    "December 2021 - December 2023",
+    "2023-2025",
+    "2022-2025",
+    "2022-2025"
+];
+
+let projectDescriptions = [null, null,null,
+    "The project purports to amend the nonfactual erraticism and vexing convulsions in the political history of Ancient India which has huge ramifications even in the present day and present the most plausible story of the early period of Indian history.",
+    "To comprehend the popularity of the text across multiple epochs.",
+    "To correct the mistranslations of the Ashokan edicts and string together a sequential narrative of Ashoka’s life, with special attention paid to understanding the man beneath the crown."
+];
+
+export default function Researchprojects() {
     return (
         <div className="projects">
 
-            <h1>Highlights</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h1>Research Projects</h1>
+            {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p> */}
 
             <ol>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+               {
+                names.map((name, index) => (
+                    <li><Publication pub={{facultyname: name, projecttitle: projects[index], funding_agency: fundingAgencies[index], grant_amount: fundingAmounts[index], duration: projectDurations[index] , project_description : projectDescriptions[index]}} /></li>
+                ))
+               }
             </ol>
-            
+
         </div>
     );
+}
+
+function Publication({ pub }) {
+
+    return (
+        <div className="pub">
+            <h4>{pub.facultyname}</h4>
+            <p>Project Title :- {pub.projecttitle}</p>
+            <p>Funding Agency :- {pub.funding_agency} </p>
+            <p> Grant Amount :- {pub.grant_amount}</p>
+            <p> Duration :- {pub.duration} </p>
+            <p> Project Description :- {pub.project_description}</p>
+
+        </div>
+    )
+
 }
--- src/pages/workshop/workshop.jsx
+++ src/pages/workshop/workshop.jsx
@@ -1,22 +1,65 @@
+let conferenceDetailsList = [
+    {
+        facultyName: "Andrea Raimondi (co-organiser)",
+        location: "Thapar",
+        date: "2021",
+        conferenceDetails: "Thought and Reality Workshop",
+        website: ""
+    },
+    {
+        facultyName: "Rabi Prakash",
+        location: "India International Centre, New Delhi",
+        date: "Jun-24",
+        conferenceDetails: "Towards an Alternative Paradigm for Bhakti: Examinations of Bhakti Texts towards its Contexts",
+        website: ""
+    },
+    {
+        facultyName: "",  // No faculty name provided
+        location: "TIET, Patiala",
+        date: "March 27-28, 2014",
+        conferenceDetails: "Exploring New Research Avenues: A Conversation among Heterodox Economics Sociology and History.",
+        website: ""
+    },
+    {
+        facultyName: "Jappen Oberoi",
+        location: "TIET, Patiala",
+        date: "March 27-28, 2024",
+        conferenceDetails: "Exploring New Research Avenues: A Conversation among Heterodox Economics Sociology and History.",
+        website: "https://www.youtube.com/@JappenOberoi/streams"
+    }
+];
+
+
 export default function Workshop() {
     return (
         <div>
             <div className="projects">
 
-                <h1>2022</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <h1>Workshop & Conference Organized</h1>
+                {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p> */}
 
                 <ol>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    {
+                        conferenceDetailsList.map((e,index)=>(
+                            <li><Publication pub={{facultyname : e.facultyName , location : e.location , date : e.date , conference : e.conferenceDetails , website : e.website}}/></li>
+                        ))
+                    }
                 </ol>
 
             </div>
         </div>
     );
+}
+
+function Publication({ pub }) {
+
+    return (
+        <div className="pub">
+            <h4>{pub.facultyname}</h4>
+            <p>Location :- {pub.location} , Date :- {pub.date} </p>
+            <p>Details of the conference :- {pub.conference}</p>
+            {pub.website == '' ? null : <a href={pub.website} target="blank">Website</a>}
+        </div>
+    )
+
 }
