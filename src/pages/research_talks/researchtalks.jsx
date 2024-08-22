60fd6b99be2fc225c2828e6a9bed36515ce39c60
mesaaransh <mesaaransh@gmail.com>
2024-08-22 15:51:19 +0530

    research talks done

--- src/pages/research_talks/researchtalks.jsx
+++ src/pages/research_talks/researchtalks.jsx
@@ -1,8 +1,41 @@
-export default function Researchtalks()
-{
+import { useEffect } from "react";
+import workData from "./data.js";
+import { useOutletContext } from "react-router-dom";
+
+export default function Researchtalks() {
+    let [setTitle, setDesc] = useOutletContext()
+
+    useEffect(() => {
+        setTitle("Research Talks")
+        setDesc("Learn from the World's Most Accomplished Researchers, Visionary Innovators, and Trailblazing Industry Leaders to Achieve Your Full Potential")
+    }, [])
+
     return (
         <div>
-            research talks
+            <div className="projects">
+
+                <h1>Research Talks</h1>
+
+                {
+                    workData.map((work) => (
+                        <Works pub={work} />
+                    ))
+                }
+
+            </div>
         </div>
     );
+}
+
+
+function Works({ pub }) {
+
+    return (
+        <div className="pub">
+            <h4>{pub.Title}</h4>
+            <p>By: {pub.FacultyName}</p>
+            <p>{pub.Organizer}, {pub.Location} </p>
+            {/* <p>{pub.Description}</p> */}
+        </div>
+    )
 }
--- src/pages/workshop/workshop.jsx
+++ src/pages/workshop/workshop.jsx
@@ -4,40 +4,6 @@ import { useEffect } from "react";
 
 export default function Workshop() {
 
-    let [setTitle, setDesc] = useOutletContext()
 
-    useEffect(() => {
-        setTitle("Workshops & Conferences")
-        setDesc("Access Exclusive, Cutting-Edge Insights: Unlock the Latest Research and Industry Trends to Drive Innovation")
-    }, [])
-
-    return (
-        <div>
-            <div className="projects">
-
-                <h1>Workshops</h1>
-
-                {
-                    workData.map((work) => (
-                        <Works pub={work}/>
-                    ))
-                }
-
-            </div>
-        </div>
-    );
-}
-
-
-function Works({pub}){
-
-    return(
-        <div className="pub">
-            <h4>{pub.Title}</h4>
-            <p>By: {pub.FacultyName}</p>
-            <p>{pub.Organizer}, {pub.Location} </p>
-            {/* <p>{pub.Description}</p> */}
-        </div>
-    )
 
 }
