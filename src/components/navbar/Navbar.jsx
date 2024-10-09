e3cc3ed2f95363e0ce01c597a7ec669431b01a3e
chirag bansal <chirageng.stu22@gmail.com>
2024-10-10 02:12:14 +0530

    homepage : background gradient added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import tslaslogo from '../../assets/tslaslogo.png'
 
 const Navbar = () => {
     return (
-        <div className=" border-t-2 select-none h-20 text-white flex justify-between items-center p-5">
-            <img src={tslaslogo} alt="Tslas Logo" className=' ml-5 h-9 w-auto' />
+        <div className=" border-b-2 select-none h-20 text-white flex justify-between items-center p-5">
+            <img src={tslaslogo} alt="Tslas Logo" className=' ml-12 h-9 w-auto ' />
             <div className=' flex h-20 items-center'>
                 <div className=" mr-12 text-2xl font-normal">HOME</div>
                 <div className=" mr-12 text-2xl font-normal">ABOUT</div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,17 +4,17 @@ import Card from "../../components/card/card";
 
 const Home = () => {
   return (
-    <div className=" select-none h-screen bg-slate-900 flex-col justify-between">
+    <div className=" bg-gradient-to-r from-slate-950 to-slate-800 select-none h-screen flex-col justify-between">
      <div className=" h-1/5"> <Navbar /></div>
       <div className=" w-full h-4/5 flex">
-        <div className=" h-full w-1/2 flex flex-wrap justify-center select-none">
-        <div className="h-1/6 w-1/4 mr-10 ml-10"><Card /></div>
-        <div className="h-1/6 w-1/4 mr-10 ml-10"><Card /></div>
-        <div className="h-1/6 w-1/4 mr-10 ml-10"><Card /></div>
-        <div className="h-1/6 w-1/4 mr-10 ml-10"><Card /></div>
+        <div className=" h-full w-1/2 flex flex-wrap justify-center select-none pt-5">
+        <div className="h-1/6 w-1/4 mr-14 ml-10"><Card /></div>
+        <div className="h-1/6 w-1/4 mr-14 ml-10"><Card /></div>
+        <div className="h-1/6 w-1/4 mr-14 ml-10"><Card /></div>
+        <div className="h-1/6 w-1/4 mr-14 ml-10"><Card /></div>
         </div>
         <div className=" h-full w-1/2 flex items-end justify-center">
-        <img src={tslas} alt="Background" className=" h-full w-10/12 pt-5"/>
+        <img src={tslas} alt="Background" className=" h-full w-full pt-5 pr-16"/>
         </div>
       </div>
     </div>
