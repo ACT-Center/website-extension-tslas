5483e6e07f42dea0445c360b8207a44fd4fb11bd
chirag bansal <chirageng.stu22@gmail.com>
2024-10-09 23:36:09 +0530

    homepage : navbar added , background image added

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,18 +1,12 @@
 import './card.css'
-import { Outlet, Link, useNavigate } from "react-router-dom";
 
-
-export default function Card({id , heading , link})
+export default function Card()
 {   
 
-    const navigator = useNavigate()
-    function clickHandler(){
-        navigator(link)
-    }
-
     return (
-        <div className="homeCard" id={id} onClick={clickHandler}>
-            <h3>{heading}</h3>
+        <div>
+            hewllo
         </div>
+
     );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/Navbar";
 import tslas from "../../assets/tslas.jpg"
+import Card from "../../components/card/card";
 
 const Home = () => {
   return (
@@ -7,10 +8,10 @@ const Home = () => {
      <div className=" h-1/5"> <Navbar /></div>
       <div className="  w-full h-4/5 flex">
         <div className=" h-full w-1/2 ">
-        
+        <div><Card /></div>
         </div>
         <div className=" h-full w-1/2 flex items-end justify-center">
-        <img src={tslas} alt="Background" className=" h-full w-full pr-14 pt-5"/>
+        <img src={tslas} alt="Background" className=" h-full w-10/12 pr-10 pt-5"/>
         </div>
       </div>
     </div>
